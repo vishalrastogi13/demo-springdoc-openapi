0103ef90e6100d8c5ede45b4ccf889f21444fab0
Vishal Rastogi <vishalrastogi@sharechat.co>
2022-05-26 09:19:06 +0530

    added apis for openapi demo

---
+++ src/main/java/org/springdoc/demo/services/book/controller/BookController.java
@@ -0,0 +1,138 @@
+/*
+ *
+ *  * Copyright 2019-2020 the original author or authors.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      https://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+
+package org.springdoc.demo.services.book.controller;
+
+import java.awt.print.Pageable;
+import java.util.Collection;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springdoc.api.annotations.ParameterObject;
+import org.springdoc.demo.services.book.exception.BookNotFoundException;
+import org.springdoc.demo.services.book.model.Book;
+import org.springdoc.demo.services.book.repository.BookRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+@SecurityScheme(name = "api", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
+public class BookController {
+
+	@Autowired
+	private BookRepository repository;
+
+	@Operation(summary = "Get a book by its id", security = {@SecurityRequirement(name = "api")})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the book",
+					content = { @Content(mediaType = "application/json",
+							schema = @Schema(implementation = Book.class)) }),
+			@ApiResponse(responseCode = "400", description = "Invalid id supplied",
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "Book not found",
+					content = @Content) })
+	@GetMapping("/{id}")
+	public Book findById(@PathVariable long id) {
+
+		//demo - adding the book
+		Book book = new Book();
+		book.setId(id);
+		book.setTitle("Demo book");
+		book.setAuthor("Demo Author");
+		repository.add(book);
+
+		return repository.findById(id)
+				.orElseThrow(BookNotFoundException::new);
+	}
+
+	@Operation(summary = "Get all books", security = {@SecurityRequirement(name = "api")})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Found the books",
+					content = { @Content(mediaType = "application/json",
+							array = @ArraySchema(schema = @Schema(implementation = Book.class))) }),
+			@ApiResponse(responseCode = "400", description = "Bad request",
+					content = @Content),
+			@ApiResponse(responseCode = "404", description = "No Book found",
+					content = @Content) })
+	@GetMapping("/")
+	public Collection<Book> findBooks() {
+		return repository.getBooks();
+	}
+
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Book updateBook(@PathVariable("id") final String id, @RequestBody final Book book) {
+		return book;
+	}
+
+	@PatchMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Book patchBook(@PathVariable("id") final String id, @RequestBody final Book book) {
+		return book;
+	}
+
+	@PostMapping("/")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Book postBook(@NotNull @Valid @RequestBody final Book book) {
+		return book;
+	}
+
+	@RequestMapping(method = RequestMethod.HEAD, value = "/")
+	@ResponseStatus(HttpStatus.OK)
+	public Book headBook() {
+		return new Book();
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public long deleteBook(@PathVariable final long id) {
+		return id;
+	}
+
+	@GetMapping("/filter")
+	@ResponseStatus(HttpStatus.OK)
+	public Page<Book> filterBooks(@ParameterObject Pageable pageable) {
+		return repository.getBooks(pageable);
+	}
+}
